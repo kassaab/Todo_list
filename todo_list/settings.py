186a1724be0bd01dda2f9b25c24636b945b57366
kassaab <abrhamkassa12@gmail.com>
2023-12-20 20:47:38 -0800

    complete

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo_list_app.apps.TodoListAppConfig',
+    'corsheaders',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://your-frontend-domain.com",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'todo_list.urls'
