5721ccfdc67b9ba15c089b7a34270422ad802821
kassaab <abrhamkassa12@gmail.com>
2024-02-10 20:18:40 -0800

    static configeration updated

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -139,7 +139,7 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
